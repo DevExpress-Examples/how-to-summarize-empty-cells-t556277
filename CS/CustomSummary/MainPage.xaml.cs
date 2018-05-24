dc069b413ea6d228a60099bb6d94a9c4a2d66bff
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-05-24 16:40:13 +0300

    initial commit

---
+++ CS/CustomSummary/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using DevExpress.Data;
+using DevExpress.UI.Xaml.Grid;
+using Windows.UI.Xaml.Controls;
+
+namespace CustomSummary {
+    public sealed partial class MainPage : Page {
+        List<TestData> list;
+        Dictionary<int, bool> selectedValues = new Dictionary<int, bool>();
+
+        public MainPage() {
+            this.InitializeComponent();
+            list = new List<TestData>();
+            for (int i = 0; i < 100; i++) {
+                list.Add(new TestData() { Text = "item " + i, Number = i });
+                if (i % 3 == 0)
+                    list[i].Number = null;
+            }
+            grid.ItemsSource = list;
+        }
+
+        int emptyCellsTotalCount = 0;
+
+
+
+        public class TestData {
+            public string Text { get; set; }
+            public int? Number { get; set; }
+        }
+
+        private void grid_CustomSummary(object sender, DevExpress.Data.CustomSummaryEventArgs e) {
+            if (((GridSummaryItem)e.Item).FieldName != "Number")
+                return;
+            if (e.IsTotalSummary) {
+                if (e.SummaryProcess == CustomSummaryProcess.Start) {
+                    emptyCellsTotalCount = 0;
+                }
+                if (e.SummaryProcess == CustomSummaryProcess.Calculate) {
+                    int? val = (int?)e.FieldValue;
+                    if (!val.HasValue)
+                        emptyCellsTotalCount++;
+                    e.TotalValue = emptyCellsTotalCount;
+                }
+            }
+
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to: Summarize Empty Cells
+
+
+<p>The following example demonstrates how to use custom summaries to count the total number of empty cells in a grid column.</p>
+
+<br/>
+
+
